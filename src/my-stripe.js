d8d56a39f75821450667957b025b08c0df730e60
carlitosalli <carlitosalvarezlinares@gmail.com>
2019-07-05 17:52:10 +0200

    Upload stripe 2

--- src/my-stripe.js
+++ src/my-stripe.js
@@ -8,11 +8,27 @@ import '@polymer/iron-icons/iron-icons.js';
 class MyStripe extends PolymerElement {
   static get properties() {
     return {
+      stripe: { value: Stripe('pk_test_MzZvwHo2zlzC6iFCZqLWXc60')}
     };
   }
-  stripeCalling () {
-    Stripe('pk_test_MzZvwHo2zlzC6iFCZqLWXc60').redirectToCheckout({
-      items: [{ sku: 'sku_FK8ojS0i3qW24x', quantity: 1 }],
+  // stripeCalling () {
+  //   Stripe('pk_test_MzZvwHo2zlzC6iFCZqLWXc60').redirectToCheckout({
+  //     items: [{ sku: 'sku_FK8ojS0i3qW24x', quantity: 1 }],
+  //     successUrl: 'http://127.0.0.1:8081/success',
+  //     cancelUrl: 'http://127.0.0.1:8081/canceled',
+  //   })
+  //   .then(function (result) {
+  //     if (result.error) {
+  //       var displayError = document.getElementById('error-message');
+  //       displayError.textContent = result.error.message;
+  //     }
+  //   });
+  // }
+  clickToBuy(e){
+    console.log(e.target.id);
+    var itemId = e.target.id;
+    this.stripe.redirectToCheckout({
+      items: [{ sku: itemId, quantity: 1 }],
       successUrl: 'http://127.0.0.1:8081/success',
       cancelUrl: 'http://127.0.0.1:8081/canceled',
     })
@@ -23,9 +39,10 @@ class MyStripe extends PolymerElement {
       }
     });
   }
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
+    // this.addEventListener('click', this.clickToBuy);
+  }
   static get template() {
     return html`
       <style include="shared-styles">
@@ -41,6 +58,10 @@ class MyStripe extends PolymerElement {
           background-color: #00c853;
           color: white;
         }
+        paper-button[disabled]{
+          background-color: #eaeaea;
+          color: #a8a8a8;
+        }
       </style>
 
       <div class="card">
@@ -50,7 +71,20 @@ class MyStripe extends PolymerElement {
 
         <div id="error-message"></div>
         
-        <paper-button raised on-click="stripeCalling">
+        <paper-button raised on-click="stripeCalling" disabled>
+          <iron-icon icon="payment"></iron-icon>
+          Pay
+        </paper-button>
+
+        <hr>
+        Item 1
+        <paper-button raised id="sku_FK8ojS0i3qW24x" on-click="clickToBuy">
+          <iron-icon icon="payment"></iron-icon>
+          Pay
+        </paper-button>
+        <hr>
+        Item 2
+        <paper-button raised id="sku_FNf1Km4MaGLIWn" on-click="clickToBuy">
           <iron-icon icon="payment"></iron-icon>
           Pay
         </paper-button>
